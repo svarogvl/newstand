d0932659b0821c98f2895ff5156829512a9b0dea
svarog <svarog.vl@gmail.com>
2014-02-20 23:20:43 +0400

    first commit

--- src/mainform.java
+++ src/mainform.java
@@ -2,4 +2,14 @@
  * Created by svarog on 20.02.14.
  */
 public class mainform {
+    public void mainform()
+    {
+        ;
+    }
+
+    public static void main(String args)
+    {
+
+    }
+
 }
