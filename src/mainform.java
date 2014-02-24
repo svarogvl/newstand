bbadb8da32329b56562fadb813dffcb7737a27bf
svarog <svarog.vl@gmail.com>
2014-02-24 23:21:45 +0400

    third commit

--- src/mainform.java
+++ src/mainform.java
@@ -1,15 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by svarog on 20.02.14.
  */
 public class mainform {
-    public void mainform()
+
+    public static void main(String[] args)
     {
-        ;
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JStandFrame frame = new JStandFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
     }
 
-    public static void main(String args)
-    {
-        ;
-    }
+}
 
+/**
+ * Class implements main form for "newstand"
+ */
+class JStandFrame extends JFrame
+{
+    public JStandFrame()
+    {
+        setTitle("Газетный киоск");
+        Dimension sizeScreen = Toolkit.getDefaultToolkit().getScreenSize();
+
+        //frame size is 60% of screen
+        int frameWidth = Math.round(sizeScreen.width*60/100);
+        int frameHeight = Math.round(sizeScreen.height*60/100);
+
+        setSize(sizeScreen.width, sizeScreen.height);
+
+    }
 }
